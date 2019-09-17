6250fa078aa391312f654134014e1864f5cf73fd
Zerek <zerek.cover@gmail.com>
2019-09-17 11:37:21 -0700

    code works, cleaned up code, acp for heroku deployment.

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const cors = require('cors');
 require('dotenv').config();
 app.use(cors());
 
-const testArray = [];
-
 //dummy data{
 // "search_query": "seattle",
 // "formatted_query": "Seattle, WA, USA",
@@ -19,12 +17,13 @@ const testArray = [];
 //creating /location route
 app.get('/location', (request, response) => {
   let searchQuery = request.query.data;
-  const geoDataResults = require('./data/geo.json');  
+  const geoDataResults = require('./data/geo.json');
 //   const darkSkyResults = require('./data/darksky.json');
 
-  const locations = new Location(searchQuery, geoDataResults);
-  response.send(locations);
-  console.log('testArray :', testArray);
+  const theLocation = new Location(searchQuery, geoDataResults);
+
+
+  response.send(theLocation);
 })
 
 // constructor
@@ -33,12 +32,7 @@ function Location(searchQuery, geoDataResults) {
   this.formatted_query = geoDataResults.results[0].formatted_address;
   this.latitude = geoDataResults.results[0].geometry.location.lat;
   this.longitude = geoDataResults.results[0].geometry.location.lng;
-
-  testArray.push(this);
 }
-
-
-
 
 
 
