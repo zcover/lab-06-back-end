7dadfe92b0e255c9fb63a15c45ae0688279bebff
Zerek <zerek.cover@gmail.com>
2019-09-17 17:45:47 -0700

    got it back to working state

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const PORT = process.env.PORT||3000;
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
 const app = express();
 app.use(cors());
+const PORT = process.env.PORT||3000;
+
 
 
 // ======= Routes ==========
@@ -19,26 +20,31 @@ app.get('/weather', getWeather);
 //--- CALLBACK FUNCTIONS ------
 
 
-//callback for /location route
-let getLocation = (request, response) => {
+//callback for /location route *needs to be regular funcion, NOT arrow => function
+function getLocation(request, response){
   let searchQuery = request.query.data;
+  console.log(searchQuery)
+  let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchQuery}&key=${process.env.GEOCODE_API_KEY}`
 
-  let url = `https://maps.googleapis.com/maps/api/gecode/jason?address=${searchQuery}&key=${process.env.GEOCODE_API_KEY}`;
+  superagent.get(url).then(superagentResults => {
+    let results = superagentResults.body.results[0];
+    // console.log('results: ', results)
+    const formatted_address = results.formatted_address;
+    const lat = results.geometry.location.lat;
+    const long = results.geometry.location.lng;
+    const newLocation = new Location(searchQuery, formatted_address, lat, long);
 
-  superagent.get(url)
-    .then(superagentResults => {
-      let results = superagentResults.body.results[0];
-      const formatted_address = results.formatted_address;
-      const lat = results.geometry.location.lat;
-      const long = results.geometry.location.lng;
-      const location = new Location(searchQuery, formatted_address, lat, long);
 
-      response.send(location);
-    })
+    response.send(newLocation);
+  }).catch(error => {
+    response.status(500).send(error.message);
+    console.error(error);
+  })
+
 }
 
 //callback for /weather route
-let getWeather = (request, response) => {
+function getWeather(request, response){
   const darkSkyResults = require('./data/darksky.json');
   let darkskyDataArray = darkSkyResults.daily.data;
   const dailyArray = darkskyDataArray.map(day => {
