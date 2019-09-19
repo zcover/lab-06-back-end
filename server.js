c4864646ecd3747ea5a419ef2cc5f766479cb06f
Zerek <zerek.cover@gmail.com>
2019-09-19 08:57:39 -0700

    not sure why it isn't working- replaced eventbrite api with public (was private)

--- server.js
+++ server.js
@@ -14,9 +14,7 @@ const PORT = process.env.PORT || 3001;
 
 app.get('/location', getLocation);
 app.get('/weather', getWeather);
-//  DOES NOT WORK !!!!
-
-// app.get('/events', getEvents)
+app.get('/events', getEvents)
 
 // =========================
 
@@ -38,7 +36,7 @@ function getLocation(request, response) {
     const long = results.geometry.location.lng;
     const newLocation = new Location(searchQuery, formatted_address, lat, long);
 
-
+    console.log('sending location')
     response.send(newLocation);
   }).catch(error => {
     console.log('=====================halp');
@@ -47,16 +45,20 @@ function getLocation(request, response) {
   })
 }
 function getWeather(request, response) {
-//   console.log('This is the response derp :');
+  console.log('first line of getweather')
+  //   console.log('This is the response derp :');
   let lat = request.query.data.latitude;
   let long = request.query.data.latitude;
   const weatherUrl = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${lat},${long}`;
+
+  console.log('about to send superagent for weather')
   superagent.get(weatherUrl).then(superagentResults => {
+    console.log('inside of superagent for weather')
     let darkskyDataArray = superagentResults.body.daily.data;
     const dailyArray = darkskyDataArray.map(day => {
       return new Weather(day)
     });
-    
+    console.log('sending weather')
     response.send(dailyArray);
   }) .catch(error => {
     response.status(500).send(error.message);
@@ -65,35 +67,50 @@ function getWeather(request, response) {
 }
 function getEvents(request, response) {
   let long = request.query.data.longitude;
+  // console.log('longitude: ',long)
   let lat = request.query.data.latitude;
+  // console.log('latitude: ',lat)
+
 
   let url = `https://www.eventbriteapi.com/v3/events/search?location.longitude=${long}&location.latitude=${lat}&token=${process.env.EVENTS_API_KEY}`;
 
-  superagent.get(url).then(superagentEventResults => {
-    console.log(superagentEventResults);
+  superagent.get(url).then(superEventResults => {
+    console.log('inside events superagent')
+    console.log(superEventResults.body)
 
-
+    const eventBriteinfo = superEventResults.body.events.map(eventData => {
+      const event = new Event(eventData);
+      return event;
+    })
+    console.log('sending event')
+    response.send(eventBriteinfo)
   }).catch(error => {
     response.status(500).send(error.message)
   })
 
 
 }
+
 // ------ END -----
 
 //-------- constructor funtions
+
 function Location(searchQuery, formatted_address, lat, long) {
   this.search_query = searchQuery;
   this.formatted_query = formatted_address;
   this.latitude = lat;
   this.longitude = long;
 }
-
 function Weather(darkskyData) {
   this.time = new Date(darkskyData.time * 1000).toDateString();
   this.forecast = darkskyData.summary;
 }
-
+function Event(eventData){
+  this.link = eventData.url;
+  this.name = eventData.name;
+  this.event_date = new Date(eventData.start.local).toDateString();
+  this.summary = eventData.summary
+}
 // --------- END --------
 
 
